0c61628a5b06e96c033c176859dc2affa4433171
Sam <samanthakpower@gmail.com>
2016-11-22 16:34:10 -0500

    Connect Rails/Ember

---
+++ app/adapters/application.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+export default DS.JSONAPIAdapter.extend({
+  namespace: 'api/v1',
+  headers: Ember.computed(function() {
+    return {
+      'accept': 'application/json',
+      'content-type': 'application/json',
+    };
+  })
+});
