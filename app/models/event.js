32fb37347562f71b95383d0429c58a3f25f34c76
Sam <samanthakpower@gmail.com>
2016-12-09 20:03:58 -0500

    Client side error handling

--- app/models/event.js
+++ app/models/event.js
@@ -1,12 +1,23 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+const { attr } = DS;
+const { computed } = Ember;
 
 export default DS.Model.extend({
-  start: DS.attr('utc'),
-  end: DS.attr('utc'),
-  name: DS.attr('string'),
-  title: Ember.computed.alias('name'),
-  fullCalendarFormat: Ember.computed('start', 'end', 'name', 'id', function() {
+  start: attr('utc'),
+  end: attr('utc'),
+  name: attr('string'),
+  title: computed.alias('name'),
+  date: computed('start', function() {
+    return this.get('start').format('YYYY-MM-DD');
+  }),
+  startTime: computed('start', function() {
+    return this.get('start').format('h:mma');
+  }),
+  endTime: computed('end', function() {
+    return this.get('end').format('h:mma');
+  }),
+  fullCalendarFormat: computed('start', 'end', 'name', 'id', function() {
     return {
       title: this.get('name'),
       start: this.get('start'),
