0763dc1090f726e7e8073acf99b47a1c52034eae
Sam <samanthakpower@gmail.com>
2016-12-26 20:23:11 -0500

    Restore Oauth session on refresh

--- app/authenticators/torii.js
+++ app/authenticators/torii.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import Torii from 'ember-simple-auth/authenticators/torii';
 
-const { inject: { service }, $ } = Ember;
+const { inject: { service }, $, merge, RSVP } = Ember;
 
 export default Torii.extend({
   torii: service('torii'),
@@ -9,16 +9,26 @@ export default Torii.extend({
   store: service('store'),
 
   authenticate(options) {
-    return this._super(options).then((data) => {
+    return this._super(options).then((providerData) => {
       return $.ajax({
         type: "POST",
         // TODO: code as http param as temporary fix for issues with Omniauth middleware
-        url: `http://localhost:3000/users/auth/google_oauth2/callback?code=${data.authorizationCode}`,
+        url: `http://localhost:3000/users/auth/google_oauth2/callback?code=${providerData.authorizationCode}`,
         contentType: "application/json; charset=utf-8",
-        data: JSON.stringify({ code: data.authorizationCode }),
+        data: JSON.stringify({ code: providerData.authorizationCode }),
         dataType: "json",
         headers: { 'X-Requested-With': 'XMLHttpRequest' }
       });
+    }).then((internalData) => {
+      // TODO: fix this workaround used to invalidate session
+      internalData.provider = this._provider;
+      return internalData;
     });
+  },
+
+  restore(data) {
+    var resolveData = data || {};
+    this._provider = resolveData.provider;
+    return new RSVP.Promise(function(resolve) { resolve(resolveData); });
   }
 });
